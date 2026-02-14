5e98833d1e62d76d839fceabf9780be5911b6b69
silver115 <silver115@users.noreply.github.com>
2026-02-15 12:45:17 +1300

    Fix EAS build: restore public identifiers in app.json, commit google-services.json, dismiss notifications on Feed tab

--- app.config.js
+++ app.config.js
@@ -23,20 +23,6 @@ if (fs.existsSync(envPath)) {
 const appJson = require("./app.json");
 const config = appJson.expo || appJson;
 
-// Override sensitive fields if .env values exist
-if (env.EXPO_OWNER) {
-  config.owner = env.EXPO_OWNER;
-}
-if (env.EAS_PROJECT_ID) {
-  config.extra = config.extra || {};
-  config.extra.eas = config.extra.eas || {};
-  config.extra.eas.projectId = env.EAS_PROJECT_ID;
-}
-if (env.ANDROID_PACKAGE) {
-  config.android = config.android || {};
-  config.android.package = env.ANDROID_PACKAGE;
-}
-
 // Inject app-runtime secrets into extra so code can read them via Constants.expoConfig.extra
 config.extra = config.extra || {};
 config.extra.pushServerUrl = env.PUSH_SERVER_URL || "";
